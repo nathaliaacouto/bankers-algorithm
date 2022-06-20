97c9733bfc58f11315e7f6fdb0937dfe39867606
unknown <nvc@cesar.school>
2022-06-20 12:04:35 -0300

    só precisa adicionar o estado do sistema

--- nvc/nvc.c
+++ nvc/nvc.c
@@ -3,21 +3,17 @@
 
 int main(int argc, char *argv[ ]) 
 {  
-  /* Declarations - Begin */
   //Get the number of resources
   number_of_resources = argc - 1;
 
+  //Get number of costumers from file lines
+  FILE *costumers = fopen("customer.txt", "r");
+  error_open_file(costumers);
+  number_of_costumers = count_file_lines(costumers);
+  fclose(costumers);
+
   //Allocate array available
   available = (int*)malloc(number_of_resources*sizeof(int));
-
-  //Number of resources the costumers still need
-  int need[number_of_costumers][number_of_resources]; //need = max - allocation
-  //Number of resources allocated to each costumer
-  int allocation[number_of_costumers][number_of_resources];
-  //Max number of resources each costumer will need
-  int maximum[number_of_costumers][number_of_resources];
-  /* Declarations - End */
-
 
   /* Initialize Matrices and Variables - Begin */
   int count_av = 0;
@@ -26,13 +22,22 @@ int main(int argc, char *argv[ ])
     available[count_av] = atoi(argv[i]);
     count_av++;
   }
+
+  for(int i = 0; i < number_of_resources; i++) {
+    printf(" %d ", available[i]);
+  }
+
+  /* Declarations - Begin */
   
-  //Get number of costumers from file lines
-  FILE *costumers = fopen("customer.txt", "r");
-  error_open_file(costumers);
-  number_of_costumers = count_file_lines(costumers);
-  fclose(costumers);
+  //Number of resources the costumers still need
+  int need[number_of_costumers][number_of_resources]; //need = max - allocation
+  //Number of resources allocated to each costumer
+  int allocation[number_of_costumers][number_of_resources];
+  //Max number of resources each costumer will need
+  int maximum[number_of_costumers][number_of_resources];
   
+  /* Declarations - End */
+
   //Get number of commands from file lines
   FILE *commands = fopen("commands.txt", "r");
   error_open_file(commands);
@@ -44,7 +49,7 @@ int main(int argc, char *argv[ ])
       allocation[i][j] = 0;
     }
   }
-
+  
   /* Get Max resources - Begin */
   /* The number of maximum resources for each 
   costumer is given in the costumer.txt file */
@@ -53,7 +58,7 @@ int main(int argc, char *argv[ ])
   for(int i = 0; i < number_of_costumers; i++) {
     for(int j = 0; j < number_of_resources; j++) {
       fscanf(cos, "%d,", &read_costumer);
-      maximum[i][j] = read_costumer; //ERRO: PEGANDO OS VALORES TUDO 0
+      maximum[i][j] = read_costumer; 
     }
   }
   fclose(cos);
@@ -66,7 +71,7 @@ int main(int argc, char *argv[ ])
     }
   }
   /* Initialize Matrices and Variables - End */
-
+ 
   /* Read Commands - Begin */
   FILE *result = fopen("result.txt", "w");
   FILE *com = fopen("commands.txt", "r");
@@ -75,11 +80,11 @@ int main(int argc, char *argv[ ])
   while(count_commands != number_of_commands) { 
     fgets(commands_arr, 20, com); 
     /* Read Commands - End */
-
+    //O ARQUIVO RESULT ESTÁ DIFERENTE DO DELE PQ AINDA NÃO DEI RELEASE!!!
     /* Do Commands - Begin */
     int exit = 0;
     int verify = verify_command(commands_arr); 
-    if(verify == REQUEST_RESOURCES) { 
+    if(verify == REQUEST_RESOURCES) { //request está funcionando
       int index = 3; 
       int client = commands_arr[index] - '0'; 
       int req[number_of_resources+1]; 
@@ -91,9 +96,9 @@ int main(int argc, char *argv[ ])
 
       int c = 0;
       for(int j = 1; j < number_of_resources + 1; j++) {
-        if(req[j] >= need[client][c]) { //preciso verificar todos os requisitos
+        if(req[j] > need[client][c]) { 
           fprintf(result, "The costumer %d request ", req[0]);
-          for(int a = 0; a < number_of_resources; a++) {
+          for(int a = 1; a < number_of_resources + 1; a++) {
             fprintf(result, "%d ", req[a]);
           }
           fprintf(result, "was denied because exceed its maximum allocation\n");
@@ -103,7 +108,7 @@ int main(int argc, char *argv[ ])
         c++;
       }
       
-      if(exit != 1) {
+      if(exit == 0) {
         c = 0;
         int count_av = 0;
         for(int j = 1; j < number_of_resources + 1; j++) {
@@ -117,33 +122,57 @@ int main(int argc, char *argv[ ])
         if(count_av == number_of_resources) {
           for(int j = 1; j < number_of_resources + 1; j++) {
             available[c] = available[c] - req[j]; //ok
-            allocation[client][c] = allocation[client][c] + req[j];
-            need[client][c] = need[client][c] - req[j]; 
+            allocation[client][c] = allocation[client][c] + req[j]; 
+            need[client][c] = maximum[client][c] - allocation[client][c];
             c++;
           }
           fprintf(result, "Allocate to customer %d the resources %d %d %d\n", req[0], req[1], req[2], req[3]);
         }
-      }
-
-      c = 0;
-        /*
-        else if(req[j] > available[c]) {
+        else {
           fprintf(result, "The resources ");
-          for(int a = 0; a < number_of_resources; a++) {
-            fprintf(result, "%d ", available[a]);
+          for(int i = 0; i < number_of_resources; i++) {
+            fprintf(result, "%d ", available[i]);
           }
           fprintf(result, "are not enough to customer %d request ", req[0]);
-          for(int a = 0; a < number_of_resources; a++) {
+          for(int a = 1; a < number_of_resources + 1; a++) {
             fprintf(result, "%d ", req[a]);
           }
+          fprintf(result, "\n");
         }
-        */
-        c++;
-        exit = 0;
+      }
+      c = 0;
+      c++;
+      exit = 0;
     }
     else if(verify == RELEASE_RESOURCES) {
-      //
+      int index = 3; 
+      int client = commands_arr[index] - '0'; 
+      int req[number_of_resources+1]; 
+      req[0] = client;
+      for(int i = 1; i < number_of_resources + 1; i++) {
+        index = index + 2; //jump space between numbers
+        req[i] = commands_arr[index] - '0'; 
+      }
+      int c = 0;
+      for(int i = 1; i < number_of_resources + 1; i++) {
+        available[c] = available[c] + req[i]; //Return the resources to available
+        c++;
+      }
+      c = 0;
+      for(int i = 1; i < number_of_resources + 1; i++) {
+        allocation[client][c] = allocation[client][c] - req[i];
+        need[client][c] = maximum[client][c] - allocation[client][c];
+        c++;
+      }
+
+      fprintf(result, "Release from costumer %d the resources ", req[0]);
+      for(int a = 1; a < number_of_resources + 1; a++) {
+        fprintf(result, "%d ", req[a]);
+      }
+      fprintf(result, "\n");
     }
+
+
     else if(verify == SHOW_VALUES) {
       fprintf(result, "MAXIMUM |ALLOCATION  |NEED\n");
       for(int i = 0; i < number_of_costumers; i++) {
